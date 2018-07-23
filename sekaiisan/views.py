7eb0d6933bf9b1acec34487b8091f70f3e0a6291
15816018 <a5816018@aoyama.jp>
2018-07-23 14:51:29 +0900

    modify

--- sekaiisan/views.py
+++ sekaiisan/views.py
@@ -4,6 +4,8 @@ import bs4
 import re
 import random
 
+#http://127.0.0.1:8000/sekaiisan/
+
 def appmain(request):
     img = None
     init_url = 'http://whc.unesco.org'
@@ -16,5 +18,6 @@ def appmain(request):
     exp = soup.find('p').getText() #概要文を取得
     title = soup.find('h6').getText() #名称を取得
     imgSrc = init_url + img['data-src'] #画像ソースを取得
+    search_url = 'https://www.bing.com/images/search?q='+ title.replace(' ','+') #画像検索URL
 
-    return render(request, 'demo/sekaiisan.html', {'name': title, 'detail': exp, 'image': imgSrc})
+    return render(request, 'demo/sekaiisan.html', {'name': title, 'detail': exp, 'image': imgSrc, 'bing_search': search_url})
